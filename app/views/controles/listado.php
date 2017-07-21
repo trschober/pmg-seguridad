0117f45c937cdccc78a470a0692ff4310d54f1e6
Manuel Muñoz <manuel.munoz00@gmail.com>
2017-07-21 11:52:54 -0400

    se agregan opciones de años

--- app/views/controles/listado.php
+++ app/views/controles/listado.php
@@ -156,6 +156,10 @@
                 <option value="2016">2016</option>
                 <option value="2015">2015</option>
                 <option value="2014">2014</option>
+                <option value="2013">2013</option>
+                <option value="2012">2012</option>
+                <option value="2011">2011</option>
+                <option value="2010">2010</option>
                 <option value="-">-</option>
               </select>
             </div>
