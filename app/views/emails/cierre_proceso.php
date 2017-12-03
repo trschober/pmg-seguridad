337c8fae4e9344159cb7f6b4c19d1d34cacdd4e4
Manuel Muñoz <manuel.munoz00@gmail.com>
2017-12-03 10:19:33 -0300

    modificaciones plantillas correo

--- app/views/emails/cierre_proceso.php
+++ app/views/emails/cierre_proceso.php
@@ -2,7 +2,8 @@
 <html>
 	<body>
 		<p>Estimados(as) encargados(as) de SSI,</p>
-		<p> Con fecha <?=date("d-m-Y") ?>, informamos la validación y envío a la Red de Expertos la información del indicador de Seguridad de la Información 2017, correspondiente al hito de revisión intermedia, Adjuntamos documento, correspondiente a los valores efectivos informados por el Servicio en el hito de revisión.</p>
+		<p> Con fecha <?=date("d-m-Y") ?>, confirmamos que su Servicio ha informado exitosamente a la Red de Expertos, en el marco del proceso de evaluación del Indicador SSI 2017.</p>
+		<p> Adjuntamos Certificado emitido por la Red de Expertos, correspondiente a los valores efectivos informados por el Servicio, y el Informe de cumplimiento con detalle de los controles y documentos adjuntos.</p>
 		<p>Saludos,<br>
 		Plataforma SSI</p>
 	</body>
--- app/views/emails/rechazo_validador.php
+++ app/views/emails/rechazo_validador.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<body>
-		<p>Estimado(a) encargado(a) de ingresar la información de SSI,</p>
-		<p> Con fecha <?=date("d-m-Y") ?>, informamos que el perfil validador ha rechazado la información del indicador de Seguridad de la Información 2017 enviada para su validación, correspondiente al hito de revisión intermedia. Debiendo usted repetir el proceso, es decir, enviar nuevamente para su validación.</p>
+		<p>Estimado(a) encargado(a) de SSI, perfil ingreso,</p>
+		<p> Con fecha <?=date("d-m-Y") ?>, informamos a Usted que el perfil validador ha rechazado la información del indicador de Seguridad de la Información, enviada para su validación en el marco del proceso de Evaluación del Indicador 2017.</p>
+		<p>Por tal motivo, Usted debe repetir el proceso, corregir la información, y enviar nuevamente al perfil validador.</p>
 		<p>Saludos,<br>
 		Plataforma SSI</p>
 	</body>
